7c31c90cf1c0c0aa8a4c5cbda0cf27010c7161b3
Michał Dobosz <doboszmichal4@gmail.com>
2024-12-15 22:01:22 +0100

    Program do etykietowania

---
+++ etykietowanie.py
@@ -0,0 +1,13 @@
+import cv2
+
+if __name__ == "__main__":
+    img_path = "./zdjecia/zdjecie10.png"
+    img = cv2.imread(img_path)
+    
+    resized_img = cv2.resize(img, (1920, 1080))
+    
+    x, y, w, h = cv2.selectROI("Select ROI", resized_img, fromCenter=False, showCrosshair=False)
+
+    print(f"[{x}, {y}, {x + w}, {y + h}]")
+    
+    cv2.destroyAllWindows()
